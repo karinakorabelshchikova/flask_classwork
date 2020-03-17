85da44fe018b6edce5ff2069eb388de19fd611a2
Karina <podkovyoptom@mail.ru>
2020-03-17 13:14:03 +0300

    Автоматический ответ

--- старт.py
+++ старт.py
@@ -219,5 +219,19 @@ def training(prof):
     return render_template('training.html', profession=prof)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    parameters = {'title': 'Миссия 1',
+                  'surname': 'Понтий',
+                  'name': 'Пилат',
+                  'education': '?',
+                  'profession': 'прокуратор',
+                  'sex': 'мужской',
+                  'motivation': '_-*-_',
+                  'ready': True}
+    return render_template('auto_answer.html', **parameters)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
